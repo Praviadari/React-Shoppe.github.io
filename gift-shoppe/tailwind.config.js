b5ade4bd0c1e38a2e69742da004a54afc00186d4
Praviadari <91617978+Praviadari@users.noreply.github.com>
2024-07-21 15:40:20 +0530

    update

---
+++ gift-shoppe/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'site' :'#e3e6f3' ,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
